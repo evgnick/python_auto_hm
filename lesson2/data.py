7c77a8d2dc7ba6fa44749dde53f2d5af8389015d
Evgenii Nick <evgenii.tst@gmail.com>
2024-04-04 17:27:50 +0300

    Refactoring based on lecture 2

---
+++ lesson2/data.py
@@ -0,0 +1,18 @@
+# Auth positive
+LOGIN_POS = 'standard_user'
+PASSWORD_POS = 'secret_sauce'
+
+# Customer
+FIRST_NAME = "Ivan"
+LAST_NAME = "Ivanov"
+POSTAL_CODE = "300000"
+
+# URLS
+MAIN_PAGE = 'https://www.saucedemo.com/'
+PRODUCTS_PAGE = f'{MAIN_PAGE}inventory.html'
+ITEM_PAGE = f'{MAIN_PAGE}inventory-item.html'
+CART_PAGE = f'{MAIN_PAGE}cart.html'
+SAUCELABS_PAGE = 'https://saucelabs.com/'
+
+# Error message main page
+ERROR_MESSAGE = "Epic sadface: Username and password do not match any user in this service"
